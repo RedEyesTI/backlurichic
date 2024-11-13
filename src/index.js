a7c9353f294b6b6244655e9b8261fa9c66bae6d1
Jair Lurita <jairxll06@gmail.com>
2024-11-13 12:50:36 -0500

    configurando comando start v4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const app = require('./app');
-
+const PORT = 5002;
+/*
 app.listen(app.get('port'),() => {
     console.log("Servidor escuchando el puerto", app.get("port"));
+})
+*/
+app.listen(PORT,() => {
+    console.log("Servidor escuchando el puerto", PORT);
 })
